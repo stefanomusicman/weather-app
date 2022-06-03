5c468d2419bf775eb37db5429349915abec7eb1b
Stefano Proietti <stef.pro@hotmail.com>
2022-06-03 18:54:02 -0400

    main weather data is being rendered on screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
 import { IoSearchCircle } from 'react-icons/io5';
+import { BsSunFill } from 'react-icons/bs';
 
 const Home: NextPage = () => {
   
@@ -17,6 +18,10 @@ const Home: NextPage = () => {
     setCity('');
   }
   console.log(weatherData);
+
+  // let weather:string = weatherData.weather[0].main;
+  // let temperature:number = weatherData.main.temp;
+  // let feelsLike: number = weatherData.main.feels_like;
   
   return (
     <div className={styles.primary}>
@@ -24,8 +29,29 @@ const Home: NextPage = () => {
         <input type="text" value={city} placeholder='Enter City' onChange={(event) => setCity(event.target.value)}/>
         <IoSearchCircle onClick={fetchData} className={styles.searchIcon}/>
       </div>
-      <div>
+      <div style={{ visibility: !weatherData ? 'hidden' : 'visible' }} className={styles.mainWeatherContainer}>
+        <div className={styles.mainWeather}>
+          <div className={styles.weather}>
+            {weatherData.name ? <h1>{weatherData.name}</h1> : null}
+            <BsSunFill className={styles.weatherIcon}/>
+            {weatherData.weather ? <h3>{weatherData.weather[0].main}</h3> : null}
+          </div>
+          <div className={styles.temperature}>
+            <div className={styles.smallBox}>
+              <h4>Temperature</h4>
+              {weatherData.main ? <h3>{weatherData.main.temp}F</h3> : null}
+            </div>
+            <div className={styles.smallBox}>
+              <h4>Feels Like</h4>
+              {weatherData.main ? <h3>{weatherData.main.feels_like}F</h3> : null}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.secondaryWeatherContainer}>
+        <div className={styles.secondaryWeather}>
 
+        </div>
       </div>
     </div>
   )
